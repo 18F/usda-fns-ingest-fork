d96dea0d1e2017b008b31bd0a0e8eb4c5462b94c
Lindsay Young <lindsayn.young@gsa.gov>
2023-09-27 13:14:31 -0400

    Running Django 2

--- usda_fns_ingestor/usda_fns_ingestor/urls.py
+++ usda_fns_ingestor/usda_fns_ingestor/urls.py
@@ -1,24 +1,16 @@
 """usda_fns_ingestor URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 """
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
-import data_ingest.urls
+from data_ingest import urls
+
+app_name = 'usda_fns_ingestor'
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^data_ingest/', include(data_ingest.urls, namespace="data_ingest")),
-    url('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('data_ingest/', include(urls, namespace='data_ingest')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
